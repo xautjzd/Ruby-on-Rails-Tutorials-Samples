93081e80f539e793870119f4a72d313939429a2a
xautjzd <xautjzd@gmail.com>
2013-08-20 16:37:16 +0800

    update user_pages_spec.rb

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
 describe "UserPages" do
-  describe "GET /user_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get users_new_path
-      response.status.should be(200)
-    end
+  subject { page }
+  describe "Sign Up page" do
+	before { visit users_new_path }
+
+	it { should have_content('Sign Up') }
+	it { should have_title(full_title('Sign Up')) }
   end
 end
